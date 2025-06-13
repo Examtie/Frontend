1156aae72e55227cf52cff403488ed594d1b81ba
BreadTM <me@breadtm.xyz>
2025-06-13 22:47:10 +0700

    font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+Test AABCD446454564(@&(#^@*#^@#@*#@(#)))
+
+กน้่กรนยำรก้รนำ้กนรำไ้รดื่ำไนดไืดา่ไืดสาไำดไจำดดขำไดขดขำไดำ
